2b94399c31278098ee8c5c5b3fae3332c8d1e681
Issakov142 <82026984+Issakov142@users.noreply.github.com>
2024-03-20 18:44:56 +0500

    header creation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@
 
 function App() {
   return (
-    <div className="wrapper"></div>
+    <div className="wrapper clear">   
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png" alt="sneakers logo" />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/img/cart.svg" alt="cart" />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/img/user.svg" alt="user icon" />
+          </li>
+        </ul>
+      </header>
+      <div className="content p-40">
+        <h1>Все кроссовки</h1>
+        ....
+      </div>
+    </div>
   );
 }
 
