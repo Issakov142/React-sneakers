76f73e25e21a3fbe624ac762153eb16c2bffc293
Issakov142 <82026984+Issakov142@users.noreply.github.com>
2024-04-02 19:59:51 +0500

    key warning setted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from "react";
-import Card from "./components/Card";
-import Header from "./components/Header";
-import Drawer from "./components/Drawer";
+import React from 'react';
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
 
 function App() {
   const [items, setItems] = React.useState([]);
   const [catItems, setCartItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
-    fetch("https://65fee4edb2a18489b386b7f4.mockapi.io/items")
+    fetch('https://65fee4edb2a18489b386b7f4.mockapi.io/items')
       .then((res) => {
         return res.json();
       })
@@ -23,6 +24,10 @@ function App() {
   };
   console.log(catItems);
 
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className="wrapper clear">
       {cartOpened && (
@@ -33,23 +38,44 @@ function App() {
 
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
+          <h1>
+            {searchValue
+              ? `Поиск по запросу: "${searchValue}"`
+              : `Все кроссовки`}
+          </h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Поиск..." />
+            {searchValue && (
+              <img
+                onClick={() => setSearchValue('')}
+                className="clear cu-p"
+                src="/img/btn-remove.svg"
+                alt="Clear"
+              />
+            )}
+            <input
+              onChange={onChangeSearchInput}
+              value={searchValue}
+              placeholder="Поиск..."
+            />
           </div>
         </div>
 
         <div className="d-flex flex-wrap">
-          {items.map((item) => (
-            <Card
-              title={item.title}
-              price={item.price}
-              imgUrl={item.imageUrl}
-              onFavorite={() => console.log("Добавили в зпкладки")}
-              onPlus={(obj) => onAddToCart(obj)}
-            />
-          ))}
+          {items
+            .filter((item) =>
+              item.title.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((item, index) => (
+              <Card
+                key={index}
+                title={item.title}
+                price={item.price}
+                imgUrl={item.imageUrl}
+                onFavorite={() => console.log('Добавили в зпкладки')}
+                onPlus={(item) => onAddToCart(item)}
+              />
+            ))}
         </div>
       </div>
     </div>
