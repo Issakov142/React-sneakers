a30f1e2d026aac3fa96174082e87f8a564e7d285
Issakov142 <82026984+Issakov142@users.noreply.github.com>
2024-04-10 03:07:56 +0500

    img bug fix ghpages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,63 +1,63 @@
-import React from 'react';
-import Card from '../components/Card';
+import React from "react";
+import Card from "../components/Card";
 
-
-
-function Home ({items, searchValue, setSearchValue, onAddToCart, onAddToFavorite, onChangeSearchInput, isLoading}) {
-  
-  
+function Home({
+  items,
+  searchValue,
+  setSearchValue,
+  onAddToCart,
+  onAddToFavorite,
+  onChangeSearchInput,
+  isLoading,
+}) {
   const renderItems = () => {
     // [...Array(10)]
     const filteredItems = items.filter((item) =>
-              item.title.toLowerCase().includes(searchValue.toLowerCase())
-            )
-    
-    return (isLoading ? [...Array(10)] : filteredItems).map((item, index) => 
-              <Card
-                key={index}
-                onFavorite={(item) => onAddToFavorite(item)}
-                onPlus={(item) => onAddToCart(item)}
-                // added={isItemAdded(item && item.id)}
-                loading={isLoading}
-                {...item}
-                // title={item.title}
-                // price={item.price}
-                // imageUrl={item.imageUrl}
-                // id={item.id}
-              />
-            )
-  }
-    return (
-        <div className="content p-40">
-        <div className="d-flex align-center justify-between mb-40">
-          <h1>
-            {searchValue
-              ? `Поиск по запросу: "${searchValue}"`
-              : `Все кроссовки`}
-          </h1>
-          <div className="search-block d-flex">
-            <img src="/img/search.svg" alt="Search" />
-            {searchValue && (
-              <img
-                onClick={() => setSearchValue('')}
-                className="clear cu-p"
-                src="/img/btn-remove.svg"
-                alt="Clear"
-              />
-            )}
-            <input
-              onChange={onChangeSearchInput}
-              value={searchValue}
-              placeholder="Поиск..."
-            />
-          </div>
-        </div>
+      item.title.toLowerCase().includes(searchValue.toLowerCase())
+    );
 
-        <div className="d-flex flex-wrap">
-          {renderItems()}
+    return (isLoading ? [...Array(10)] : filteredItems).map((item, index) => (
+      <Card
+        key={index}
+        onFavorite={(item) => onAddToFavorite(item)}
+        onPlus={(item) => onAddToCart(item)}
+        // added={isItemAdded(item && item.id)}
+        loading={isLoading}
+        {...item}
+        // title={item.title}
+        // price={item.price}
+        // imageUrl={item.imageUrl}
+        // id={item.id}
+      />
+    ));
+  };
+  return (
+    <div className="content p-40">
+      <div className="d-flex align-center justify-between mb-40">
+        <h1>
+          {searchValue ? `Поиск по запросу: "${searchValue}"` : `Все кроссовки`}
+        </h1>
+        <div className="search-block d-flex">
+          <img src="img/search.svg" alt="Search" />
+          {searchValue && (
+            <img
+              onClick={() => setSearchValue("")}
+              className="clear cu-p"
+              src="img/btn-remove.svg"
+              alt="Clear"
+            />
+          )}
+          <input
+            onChange={onChangeSearchInput}
+            value={searchValue}
+            placeholder="Поиск..."
+          />
         </div>
       </div>
-    )
+
+      <div className="d-flex flex-wrap">{renderItems()}</div>
+    </div>
+  );
 }
 
 export default Home;
